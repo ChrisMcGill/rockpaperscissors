a691a6d469f4f222c2b681d13672a8b4930bf8b3
Chris <97418708+ChrisMcGill@users.noreply.github.com>
2023-08-24 19:21:22 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// play game button
+// button runs function of game
+// take input of player
+// return this as number? for comparison
+// create function getComputerChoice
+// math.random*100 use values to generate choice rps
+// check for catch cases 
+// use a let to count the winner of each round
+// best of 5 wins - declare the winner and end the loop
+
+// button ->
+// game function ->
+// player choice .>
+// computer choice ->
+// comparison and winner ->
+// rounds tallied ->
+// winner declared and loop closed 
 
