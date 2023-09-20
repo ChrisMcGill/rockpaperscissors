de3549ced0f7716e989d18a75a174ab910ce0cf7
Chris <97418708+ChrisMcGill@users.noreply.github.com>
2023-09-20 20:45:44 +0100

    first adjustements

--- script.js
+++ script.js
@@ -7,20 +7,32 @@ let pScore = 0;
 let cScore = 0;
 
 // get player choice from prompt 
-function playerInput(){
-    let getPlayerChoice = prompt("rock / paper / scissors?");
-    if (getPlayerChoice.toLowerCase() === "rock"){
-        return playerChoice = "rock";
-    } else if (getPlayerChoice.toLowerCase() === "paper"){
-        playerChoice = "paper";
-    } else if (getPlayerChoice.toLowerCase() === "scissors"){
-        playerChoice = "scissors";
-    } else {
-        alert("Please select rock, paper or scissors");
-    };
-};
+// function playerInput(){
+//     if (getPlayerChoice.toLowerCase() === "rock"){
+//         return playerChoice = "rock";
+//     } else if (getPlayerChoice.toLowerCase() === "paper"){
+//         playerChoice = "paper";
+//     } else if (getPlayerChoice.toLowerCase() === "scissors"){
+//         playerChoice = "scissors";
+//     } else {
+//         alert("Please select rock, paper or scissors");
+//     };
+// };
 
-// gets compute choice random - w
+
+buttonR.addEventListener('click', () => {
+    return playerChoice === "rock";
+});
+
+buttonP.addEventListener('click', () => {
+    return playerChoice === "paper";
+});
+
+buttonS.addEventListener('click', () => {
+    return playerChoice === "scissors";
+});
+
+// gets compute choice random
 function getComputerChoice(){
     let jumble;
     jumble = Math.floor(Math.random()*100);
@@ -57,26 +69,25 @@ function resultsCompare(){
 function finale(){
     if (pScore === 5){
         return gameResult = "V I C T O R Y !";
-    } else {
+    } else if (cScore ===5){
         return gameResult = "D E F E A T !";
+    } else{
+        return gameResult === "First to 5 wins";
     };
 };
 
 //function to run whole game
 function playGame(){
+    // playerInput();
+    getComputerChoice();
+    console.log(playerChoice);
+    console.log(computerChoice);
+    compareChoice();
+    resultsCompare();
+    console.log(resultsAnnounce); // equal bcuz undefined
 
-    while ((pScore<5) && (cScore<5)){
-        playerInput();
-        getComputerChoice();
-        console.log(playerChoice);
-        console.log(computerChoice);
-        compareChoice();
-        resultsCompare();
-        console.log(resultsAnnounce); // equal bcuz undefined
-
-        console.log("Human: " + (pScore));
-        console.log("Computer :" + (cScore));
-    };
+    console.log("Human: " + (pScore));
+    console.log("Computer :" + (cScore));
 
     finale();
     console.log(gameResult);
