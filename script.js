dd138b99219fb4e852ec99320ddce1cd6c773cf4
Chris <97418708+ChrisMcGill@users.noreply.github.com>
2023-09-21 20:05:12 +0100

    functionality mostly complete

--- script.js
+++ script.js
@@ -6,33 +6,7 @@ let gameResult;
 let pScore = 0;
 let cScore = 0;
 
-// get player choice from prompt 
-// function playerInput(){
-//     if (getPlayerChoice.toLowerCase() === "rock"){
-//         return playerChoice = "rock";
-//     } else if (getPlayerChoice.toLowerCase() === "paper"){
-//         playerChoice = "paper";
-//     } else if (getPlayerChoice.toLowerCase() === "scissors"){
-//         playerChoice = "scissors";
-//     } else {
-//         alert("Please select rock, paper or scissors");
-//     };
-// };
-
-
-buttonR.addEventListener('click', () => {
-    return playerChoice === "rock";
-});
-
-buttonP.addEventListener('click', () => {
-    return playerChoice === "paper";
-});
-
-buttonS.addEventListener('click', () => {
-    return playerChoice === "scissors";
-});
-
-// gets compute choice random
+// gets computer choice randomly
 function getComputerChoice(){
     let jumble;
     jumble = Math.floor(Math.random()*100);
@@ -48,7 +22,7 @@ function getComputerChoice(){
 //compare input vs computer choice
 function compareChoice(){
     if (computerChoice === playerChoice){
-        return resultsAnnounce = "It's a TIE :/ *cringe*";
+        return resultsAnnounce = "It's a TIE!";
     } else if (((computerChoice === "rock") && (playerChoice === "scissors")) || ((computerChoice === "paper") && (playerChoice === "rock")) || ((computerChoice === "scissors") && (playerChoice === "paper"))){
         return resultsAnnounce = "The machine has won again! >:(";
     } else if (((computerChoice === "scissors") && (playerChoice === "rock")) || ((computerChoice === "rock") && (playerChoice === "paper")) || ((computerChoice === "paper") && (playerChoice === "scissors"))){    
@@ -56,7 +30,7 @@ function compareChoice(){
     };    
 };
 
-//print out for round result
+//tally for scores
 function resultsCompare(){
         if (resultsAnnounce === "The machine has won again! >:("){
             return cScore++;
@@ -72,23 +46,64 @@ function finale(){
     } else if (cScore ===5){
         return gameResult = "D E F E A T !";
     } else{
-        return gameResult === "First to 5 wins";
+        return gameResult = "First to 5 wins";
+    };
+};
+
+// handles and announces ties
+function tieIsScore(){
+    if (resultsAnnounce === "It's a TIE!"){
+        tieScore.textContent = (`${resultsAnnounce}`);
+    } else{
+        tieScore.textContent = ('');
     };
 };
 
 //function to run whole game
 function playGame(){
-    // playerInput();
     getComputerChoice();
-    console.log(playerChoice);
-    console.log(computerChoice);
     compareChoice();
     resultsCompare();
-    console.log(resultsAnnounce); // equal bcuz undefined
-
-    console.log("Human: " + (pScore));
-    console.log("Computer :" + (cScore));
-
+    //need to breaklines so display underneath, maybe flex affecting?
+    humanScore.textContent = ((`Human: ${pScore}`) + '\n' + (`${playerChoice}`));
+    compScore.textContent = (`Computer: ${cScore}`) + "\n" + (`${computerChoice}`);
+    tieIsScore();
     finale();
-    console.log(gameResult);
+    declareWinner.textContent = (`${gameResult}`)
+    //could finish game here with function - game still runs on click
 };
+
+// buttons for setting choice and runnning function
+buttonR.addEventListener('mousedown', () => {
+    return playerChoice = "rock";
+});
+buttonP.addEventListener('mousedown', () => {
+    return playerChoice = "paper";
+});
+buttonS.addEventListener('mousedown', () => {
+    return playerChoice = "scissors";
+});
+
+//run function as won't work after return
+buttonR.addEventListener('mouseup', () => {
+    playGame();
+});
+buttonP.addEventListener('mouseup', () => {
+    playGame();
+});
+buttonS.addEventListener('mouseup', () => {
+    playGame();
+});
+
+//setting div display start values
+const humanScore = document.querySelector('#humanScore');
+humanScore.textContent = (`Human: ${pScore}`);
+
+const compScore = document.querySelector('#compScore');
+compScore.textContent = (`Computer: ${cScore}`);
+
+const tieScore = document.querySelector('#tieScore');
+tieScore.textContent = ("")
+
+const declareWinner = document.querySelector('#declareWinner')
+declareWinner.textContent = ("First to 5 wins")
